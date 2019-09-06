967d0e47d5f4214756fd37c36ee3880ed80b0d9e
Lew Cirne <cirne@newrelic.com>
2019-09-06 16:10:38 -0600

    cleanup indentations

--- nerdlets/github-about/configure-me.js
+++ nerdlets/github-about/configure-me.js
@@ -7,16 +7,16 @@ export default function ConfigureMe() {
     <p>
       Ever wondered what a Service does, or who has been working on it?
       Answer these questions and more with this GitHub integration!
-  </p>
+    </p>
     <h2>First Things First.</h2>
     <p>
       Let's get you started! Set up this Nerdpack by configuring your organization'
-    GitHub URL. It could be the public <a href="https://github.com">
-        https://github.com</a> or it could be a private GitHub enterprise instance.
-  </p>
+      GitHub URL. It could be the public <a href="https://github.com">
+      https://github.com</a> or it could be a private GitHub enterprise instance.
+    </p>
     <p>
       Edit the URL in <code>CONFIGURE_ME.js</code> and come back here when
       you've saved the file.  Don't deploy this Nerdpack without proper configration!
-  </p>
+    </p>
   </div>
 }
