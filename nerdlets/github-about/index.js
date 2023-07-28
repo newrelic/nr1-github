0c1bd46ab2a16815d07fe5c4132c57d375948f06
shahram <shahram@newrelic.com>
2023-07-28 01:04:02 -0700

    refactor: set nr-labs-components dependency to be fetched from npm repository

--- nerdlets/github-about/index.js
+++ nerdlets/github-about/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import GithubAbout from './main';
 import { Icon, nerdlet, NerdletStateContext } from 'nr1';
 
-import { HelpModal, Messages } from 'nr-labs-components';
+import { HelpModal, Messages } from '@newrelic/nr-labs-components';
 
 export default class Wrapper extends React.PureComponent {
   state = {
@@ -37,36 +37,34 @@ export default class Wrapper extends React.PureComponent {
         <NerdletStateContext.Consumer>
           {nerdletUrlState => <GithubAbout nerdletUrlState={nerdletUrlState} />}
         </NerdletStateContext.Consumer>
-        {helpModalOpen && (
-          <HelpModal
-            isModalOpen={helpModalOpen}
-            setModalOpen={this.setHelpModalOpen}
-            urls={{
-              docs: 'https://github.com/newrelic/nr1-github#readme',
-              createIssue:
-                'https://github.com/newrelic/nr1-github/issues/new?assignees=&labels=bug%2C+needs-triage&template=bug_report.md&title=',
-              createFeature:
-                'https://github.com/newrelic/nr1-github/issues/new?assignees=&labels=enhancement%2C+needs-triage&template=enhancement.md&title=',
-              createQuestion:
-                'https://github.com/newrelic/nr1-github/discussions/new/choose'
-            }}
-            ownerBadge={{
-              logo: {
-                src:
-                  'https://drive.google.com/uc?id=1BdXVy2X34rufvG4_1BYb9czhLRlGlgsT',
-                alt: 'New Relic Labs'
-              },
-              blurb: {
-                text: 'This is a New Relic Labs open source app.',
-                link: {
-                  text: 'Take a look at our other repos',
-                  url:
-                    'https://github.com/newrelic?q=nrlabs-viz&type=all&language=&sort='
-                }
+        <HelpModal
+          isModalOpen={helpModalOpen}
+          setModalOpen={this.setHelpModalOpen}
+          urls={{
+            docs: 'https://github.com/newrelic/nr1-github#readme',
+            createIssue:
+              'https://github.com/newrelic/nr1-github/issues/new?assignees=&labels=bug%2C+needs-triage&template=bug_report.md&title=',
+            createFeature:
+              'https://github.com/newrelic/nr1-github/issues/new?assignees=&labels=enhancement%2C+needs-triage&template=enhancement.md&title=',
+            createQuestion:
+              'https://github.com/newrelic/nr1-github/discussions/new/choose'
+          }}
+          ownerBadge={{
+            logo: {
+              src:
+                'https://drive.google.com/uc?id=1BdXVy2X34rufvG4_1BYb9czhLRlGlgsT',
+              alt: 'New Relic Labs'
+            },
+            blurb: {
+              text: 'This is a New Relic Labs open source app.',
+              link: {
+                text: 'Take a look at our other repos',
+                url:
+                  'https://github.com/newrelic?q=nrlabs-viz&type=all&language=&sort='
               }
-            }}
-          />
-        )}
+            }
+          }}
+        />
       </>
     );
   }
